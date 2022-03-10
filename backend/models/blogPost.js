cb4f2cbe54961b4efd1edaf120418a56a8431132
Aaron Slaughter <aslaugh1@gmail.com>
2022-03-10 16:30:56 -0500

    broke the comments and replies into nested schemas

--- backend/models/blogPost.js
+++ backend/models/blogPost.js
@@ -1,30 +1,42 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const BlogPost = new Schema(
+const ReplySchema = new Schema(
+  {
+    username: String,
+    body: String,
+    reported: { type: Boolean, default: false },
+    hidden: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+)
+
+const CommentSchema = new Schema(
+  {
+    username: String,
+    body: String,
+    replies: [ReplySchema],
+    reported: { type: Boolean, default: false },
+    hidden: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+)
+
+const BlogPostSchema = new Schema(
   {
     version: { type: Number, default: 1 },
     title: { type: String, required: true },
     imageUrl: { type: String, required: true },
     body: { type: String, required: true },
-    comments: [{
-      username: String,
-      body: String,
-      date: Date,
-      reported: { type: Boolean, default: false },
-      hidden: { type: Boolean, default: false },
-      replies: [{
-        username: String,
-        body: String,
-        date: Date,
-        reported: { type: Boolean, default: false },
-        hidden: { type: Boolean, default: false }
-      }]
-    }],
+    comments: [CommentSchema],
     hidden: { type: Boolean, default: false },
     hasExtras: { type: Boolean, default: false },
   },
   { timestamps: true }
 )
 
-module.exports = BlogPost
+module.exports = {
+  BlogPostSchema,
+  CommentSchema,
+  ReplySchema
+}
