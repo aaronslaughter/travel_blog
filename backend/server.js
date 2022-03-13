be9331220aa7ff284003264e2308a4f2cb593bd7
Aaron Slaughter <aslaugh1@gmail.com>
2022-03-13 14:11:03 -0400

    removed slash from path

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use('/api', routes)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')))
+  app.use(express.static(path.join(__dirname, 'client/build')))
   app.get('*', (req, res) => {
     res.sendFile(path.join(`${__dirname}/client/build/index.html`))
   })
