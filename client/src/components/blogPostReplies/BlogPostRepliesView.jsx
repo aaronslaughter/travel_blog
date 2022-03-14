4abc9122c908eccff0ae37c89953538e7e8d000e
Aaron Slaughter <aslaugh1@gmail.com>
2022-03-14 18:05:36 -0400

    added replies section with api calls

---
+++ client/src/components/blogPostReplies/BlogPostRepliesView.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { 
+  Avatar,
+  Accordion,
+  AccordionSection,
+  Button,
+  Chip } from 'react-rainbow-components'
+
+const BlogPostRepliesView = ({ replies, handleReport }) => {
+  return (
+    <div>
+      {
+        <Accordion>
+          {replies.map((element, index) =>
+            <AccordionSection
+              key={index}
+              label={
+                <div>
+                  {element.username}<br/>
+                  {element.body}<br/>
+                  {new Date(element.createdAt).toDateString()}
+                </div>
+              }
+              icon={
+                <Avatar
+                  initials={element.username.toUpperCase()}
+                />
+              }
+            >
+              {element.reported ?
+                <Chip
+                  label='Reported'
+                  variant='neutral'
+                  size='small'
+                /> :
+                <Button
+                  label='Report'
+                  variant='destructive'
+                  size='small'
+                  onClick={() => handleReport(element._id)}
+                />
+              }
+            </AccordionSection>
+          )}
+        </Accordion>
+      }
+      
+    </div>
+  )
+}
+
+export default BlogPostRepliesView
